773a8402083dadb5844b4e97d409801e6c6cadfb
KevinShindel <kevin.shindel@yahoo.com>
2019-08-03 00:56:00 +0300

    + bootstrap, popperjs, jquery + header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ApartmentsComponent} from './apartments/apartments.component';
+import {ApplicationComponent} from './application/application.component';
+import {ContactComponent} from './contact/contact.component';
+import {FavoritesComponent} from './favorites/favorites.component';
+import {PaymentComponent} from './payment/payment.component';
+import {AboutComponent} from './about/about.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {LoginComponent} from './auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ApartmentsComponent},
+  {path: 'application', component: ApplicationComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'favorites', component: FavoritesComponent},
+  {path: 'payment', component: PaymentComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'auth/register', component: RegisterComponent},
+  {path: 'auth/login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
+import { ApplicationComponent } from './application/application.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ContactComponent } from './contact/contact.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ApartmentsComponent,
+    ApplicationComponent,
+    PaymentComponent,
+    ContactComponent,
+    FavoritesComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
